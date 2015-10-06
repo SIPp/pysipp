b491a5651182abd1d78af1b6fa240d9a76abea9e
Tyler Goodlet <tgoodlet@gmail.com>
2015-10-05 22:22:05 -0400

    Add example usage to doc string
    Should be useful for doctests and usage. Use the package logger and add
    a `__str__` method as well.

--- pysipp/command.py
+++ pysipp/command.py
@@ -4,6 +4,9 @@ Command string rendering
 import string
 from distutils import spawn
 from copy import copy
+import utils
+
+log = utils.get_logger()
 
 
 def iter_format(item):
@@ -11,17 +14,46 @@ def iter_format(item):
 
 
 class CmdStr(object):
-    '''Build a command string from an iterable of format string tokens
+    '''Build a command string from an iterable of format string tokens.
+
+    Given a `template` (i.e. an iterable of format string specifiers), this
+    object wraps a command string and allows for `str.format` "replacement
+    fields" to be assigned using attribute access. Tokens are rendered only if
+    all replacement fields are assigned.
+
+    Ex.
+        >>> cmd = CmdStr('/usr/bin/sipp/, [
+            '{remote_host}:{remote_port}',
+            '-i {local_host}',
+            '-p {local_port}',
+            '-recv_timeout {msg_timeout}',
+            '-i {local_host}',
+        ]
+        >>> str(cmd)
+        '/usr/bin/sipp'
+
+        >>> cmd.remote_host = 'doggy.com'
+        >>> cmd.local_host = '192.168.0.1'
+        >>> cmd.render()
+        # both remote_host AND remote_port must be set to render token
+        '/usr/bin/sipp -i 192.168.0.1'
+
+        >>> cmd.remote_port = 5060
+        >>> str(cmd)
+        '/usr/bin/sipp doggy.com:5060 -i 192.168.0.1'
     '''
     def __init__(self, program, template):
         self.prog = program
-        self.template = tuple(template)  # list of tokens
+        self.template = tuple(template)  # iter of tokens
         self._params = set()
         for item in self.template:
             for _, name, fspec, conversion in iter_format(item):
                 self._params.add(name)
                 self.__dict__[name] = None
         self._init = True  # lock attribute creation
+
+    def __str__(self):
+        return self.render()
 
     def render(self):
         # build a content map of format string 'fields' to values
@@ -41,12 +73,12 @@ class CmdStr(object):
                 else:
                     tokens.append(item)
 
-            # print("fields '{}'".format(fields))
-            # print("content '{}'".format(content))
             if all(field in content for field in fields):
                 # only accept tokens for which we have all field values
                 tokens.append(item)
 
+        # log.debug("fields are '{}'".format(fields))
+        # log.debug("content is '{}'".format(content))
         return ' '.join([self.prog] + tokens).format(**content)
 
     def __setattr__(self, key, value):
@@ -62,7 +94,7 @@ class CmdStr(object):
 def SippCmd(spec, fields=None):
     '''A command string renderer for `sipp`.
 
-    Given the provided `spec` create a renderer type.
+    Given the provided `spec` create a cmd string renderer type.
     '''
     sipp = spawn.find_executable('sipp')
-    return CmdStr(sipp, spec)  # + tuple(fields) if fields else ())
+    return CmdStr(sipp, spec)
