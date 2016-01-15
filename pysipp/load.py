a47318853dd18b18e610f8290ef687d6309a82e5
Tyler Goodlet <tgoodlet@gmail.com>
2016-01-15 15:36:44 -0500

    Fix bug with pysipp_conf.py loading
    Python caches modules of the same name so always load conf.py modules
    with a unique name derived from its path in the file system. Include an
    assertion which verifies that all such modules are unique per `os.walk`
    scan.

--- pysipp/load.py
+++ pysipp/load.py
@@ -15,18 +15,16 @@ class CollectionError(Exception):
 def glob_for_scripts(directory):
     '''Find scenario xml scripts and conf.py in script dir
     '''
-    xmls = glob.iglob(directory+'/*.xml')
+    xmls = glob.iglob(directory + '/*.xml')
     # double check
     xmls = [f for f in xmls if 'xml' in os.path.splitext(f)[1]]
-    confpy = glob.glob(directory+'/pysipp_conf.py')
-    if len(confpy) == 1:
-        confpy = confpy[0]
-    elif len(confpy) > 1:
+    confpy = glob.glob(directory + '/pysipp_conf.py')
+    if len(confpy) > 1:
         raise ValueError(
             'can only be at most one pysipp_conf.py in scen directory!'
         )
         log.debug("discovered xmls:\n{}".format('\n'.join(xmls)))
-    return xmls, confpy
+    return xmls, confpy[0] if confpy else None
 
 
 def iter_scen_dirs(rootdir, dir_filter=lambda dir_name: dir_name):
@@ -36,25 +34,33 @@ def iter_scen_dirs(rootdir, dir_filter=lambda dir_name: dir_name):
     :return: an iterator over all scenario dirs yielding tuples of the form
         (<filepath (str)>, <xmlpaths (list)>, <confpypath (str)>)
     '''
+    mod_space = set()
     for path, dirnames, filenames in os.walk(rootdir):
 
         # filter the path dirs to traverse as we recurse the file system
         # (only use if you know what you're doing)
         dirnames[:] = filter(dir_filter, dirnames)
 
+        # scan for files
         path = os.path.abspath(path)
         xmls, confpy = glob_for_scripts(path)
-        if not len(xmls) and not confpy:
-            log.warn(
-                "No pysipp related files were found when searching for SIPp "
-                "scenarios under '{}'".format(path))
+
+        if not len(xmls):
+            log.debug("No SIPp xml scripts found under '{}'".format(path))
             continue
-        elif not len(xmls):
-            log.warn(
-                "No xml scripts were found when searching for SIPp scenarios "
-                "under '{}'".format(path))
+        if not confpy:
+            log.debug("No pysipp_conf.py found under '{}'".format(path))
 
         # load module sources
-        confpy = utils.load_mod(confpy, name='pysipp_confpy') if confpy else None
+        mod = utils.load_mod(
+            confpy,
+            # use unique names (as far as scendirs go) to avoid module caching
+            name='pysipp_confpy_{}'.format(os.path.dirname(confpy))
+        ) if confpy else None
 
-        yield path, xmls, confpy
+        # verify confpy mods should be unique
+        if mod:
+            assert mod not in mod_space
+            mod_space.add(mod)
+
+        yield path, xmls, mod
