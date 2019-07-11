24aac662d5ce5bacbc1b445b84c916c8c2f19c8f
Tyler Goodlet <jgbt@protonmail.com>
2019-07-10 22:16:23 -0400

    Adjust tests to match new `trio` apis

--- tests/test_launcher.py
+++ tests/test_launcher.py
@@ -1,15 +1,22 @@
 """
 Basic agent/scenario launching
+
 """
 from pysipp.agent import client
 from pysipp.agent import server
-from pysipp.launch import PopenRunner
+from pysipp.launch import (
+    TrioRunner,
+    run_all_agents,
+    SIPpFailure,
+)
+
+import trio
+import pytest
 
 
-def run_blocking(*agents):
-    runner = PopenRunner()
+def run_blocking(runner, agents):
     assert not runner.is_alive()
-    runner(ua.render() for ua in agents)
+    trio.run(run_all_agents, runner, agents)
     assert not runner.is_alive()
     return runner
 
@@ -23,24 +30,28 @@ def test_agent_fails():
     uac.recv_timeout = 1  # avoids SIPp issue #176
     uac.call_count = 1  # avoids SIPp issue #176
 
-    runner = run_blocking(uas, uac)
+    runner = TrioRunner()
+    with pytest.raises(SIPpFailure):
+        run_blocking(runner, (uas, uac))
 
     # fails due to invalid ip
-    uasproc = runner.get(timeout=0)[uas.render()]
-    assert uasproc.streams.stderr
+    uasproc = runner._procs[uas.render()]
+    print(uasproc.stderr_output)
+    assert uasproc.stderr_output
     assert uasproc.returncode == 255, uasproc.streams.stderr
 
     # killed by signal
-    uacproc = runner.get(timeout=0)[uac.render()]
-    # assert not uacproc.streams.stderr  # sometimes this has a log msg?
+    uacproc = runner._procs[uac.render()]
+    # assert not uacproc.stderr_output  # sometimes this has a log msg?
     ret = uacproc.returncode
     # killed by SIGUSR1 or terminates before it starts (racy)
     assert ret == -10 or ret == 0
 
 
 def test_default_scen(default_agents):
-    runner = run_blocking(*default_agents)
+    runner = TrioRunner()
+    runner = run_blocking(runner, default_agents)
 
     # both agents should be successful
-    for cmd, proc in runner.get(timeout=0).items():
+    for cmd, proc in runner._procs.items():
         assert not proc.returncode
---
+++ tests/test_scenario.py
@@ -0,0 +1,3 @@
+'''
+pysipp.agent module tests
+'''
